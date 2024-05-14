fa58e5705e198e9cdbe2dd2d90e55d46a250d979
AlainLL26 <132616532+AlainLL26@users.noreply.github.com>
2024-05-14 11:22:18 -0600

    Update Rectangulo_Autonomo.ino

--- Rectangulo_Autonomo.ino
+++ Rectangulo_Autonomo.ino
@@ -1,94 +1,60 @@
-#!/usr/bin/env python
+// Definimos los pines a los que están conectados los motores
+const int motor1APin = 5;
+const int motor1BPin = 6;
 
-import rospy
-from geometry_msgs.msg import Twist
-from turtlesim.msg import Pose
-from turtlesim.srv import Kill, Spawn
-from math import radians
+const int motor2APin = 9;
+const int motor2BPin = 10;
 
-class MoveTurtle:
-    def _init_(self):
-        rospy.init_node('move_turtle')
-        
-        # Suscribirse al topic de la posición de la tortuga
-        self.pose_subscriber = rospy.Subscriber('/turtle1/pose', Pose, self.pose_callback)
-        
-        # Publicar en el topic de comandos de movimiento de la tortuga
-        self.velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
-        
-        # Variables para la posición actual de la tortuga
-        self.current_x = 0
-        self.current_y = 0
+void setup() {
+  // Configuramos los pines de los motores como salidas
+  pinMode(motor1APin, OUTPUT);
+  pinMode(motor1BPin, OUTPUT);
+  pinMode(motor2APin, OUTPUT);
+  pinMode(motor2BPin, OUTPUT);
 
-    def pose_callback(self, pose):
-        # Función que se ejecuta cada vez que llega una actualización de la posición de la tortuga
-        self.current_x = pose.x
-        self.current_y = pose.y
+  // Establecemos el estado inicial de los pines de salida en LOW
+  digitalWrite(motor1APin, LOW);
+  digitalWrite(motor1BPin, LOW);
+  digitalWrite(motor2APin, LOW);
+  digitalWrite(motor2BPin, LOW);
+}
 
-    def move_forward(self, distance):
-        # Crear un mensaje de Twist para avanzar
-        move_msg = Twist()
-        move_msg.linear.x = distance
-        self.velocity_publisher.publish(move_msg)
+void loop() {
+  // Movemos hacia adelante ambos motores durante 5 segundos
+  digitalWrite(motor1APin, HIGH);
+  digitalWrite(motor2APin, HIGH);
+  delay(5000); // Movemos los motores durante 5 segundos
 
-    def rotate(self, angle):
-        # Crear un mensaje de Twist para rotar
-        rotate_msg = Twist()
-        rotate_msg.angular.z = angle
-        self.velocity_publisher.publish(rotate_msg)
+  // Detenemos ambos motores
+  digitalWrite(motor1APin, LOW);
+  digitalWrite(motor2APin, LOW);
+  delay(1000); // Esperamos un segundo
 
-    def kill_turtle(self):
-        # Llamar al servicio de eliminación de tortugas
-        rospy.wait_for_service('/kill')
-        try:
-            kill_turtle = rospy.ServiceProxy('/kill', Kill)
-            kill_turtle("turtle1")
-        except rospy.ServiceException as e:
-            print("Error al eliminar la tortuga: %s" % e)
+  // Giramos a la derecha (activando solo el motor1) durante 2 segundos
+  digitalWrite(motor1APin, LOW);
+  digitalWrite(motor1BPin, HIGH);
+  delay(2000); // Giramos durante 2 segundos
 
-    def spawn_turtle(self):
-        # Llamar al servicio de spawneado de tortugas
-        rospy.wait_for_service('/spawn')
-        try:
-            spawn_turtle = rospy.ServiceProxy('/spawn', Spawn)
-            spawn_turtle(6.7, 3.5, 0, "turtle1")
-        except rospy.ServiceException as e:
-            print("Error al spawneado de la tortuga: %s" % e)
+  // Detenemos el motor1
+  digitalWrite(motor1BPin, LOW);
+  delay(1000); // Esperamos un segundo
 
-    def move_turtle(self):
-        # Eliminar la tortuga actual
-        self.kill_turtle()
+  // Movemos hacia adelante ambos motores durante 5 segundos
+  digitalWrite(motor1APin, HIGH);
+  digitalWrite(motor2APin, HIGH);
+  delay(5000); // Movemos los motores durante 5 segundos
 
-        # Spawnear una nueva tortuga en la posición especificada
-        self.spawn_turtle()
+  // Detenemos ambos motores
+  digitalWrite(motor1APin, LOW);
+  digitalWrite(motor2APin, LOW);
+  delay(1000); // Esperamos un segundo
 
-        # Mover la tortuga a una posición específica
-        self.move_forward(1)  # Mover 2 unidades hacia adelante
-        rospy.sleep(1)        # Esperar un segundo
-        self.rotate(radians(0))  # Girar 90 grados (pi/2 radianes) en sentido antihorario
-        rospy.sleep(1)        # Esperar un segundo
-        self.move_forward(1.5)  # Mover 4 unidades hacia adelante
-        rospy.sleep(1)        # Esperar un segundo
-        self.rotate(radians(-90))  # Girar 90 grados (pi/2 radianes) en sentido antihorario
-        rospy.sleep(1)        # Esperar un segundo
-        self.move_forward(1)  # Mover 4 unidades hacia adelante
-        rospy.sleep(1)        # Esperar un segundo
-        self.rotate(radians(-90))  # Girar 90 grados (pi/2 radianes) en sentido antihorario
-        rospy.sleep(1)        # Esperar un segundo
-        self.move_forward(1.5)  # Mover 4 unidades hacia adelante
-        rospy.sleep(1)        # Esperar un segundo
-        self.rotate(radians(-90))  # Girar 90 grados (pi/2 radianes) en sentido antihorario
-        rospy.sleep(1)        # Esperar un segundo
-        self.move_forward(1)  # Mover 4 unidades hacia adelante
-        rospy.sleep(1)        # Esperar un segundo
-        self.rotate(radians(0))  # Girar 90 grados (pi/2 radianes) en sentido antihorario
-        rospy.sleep(1)        # Esperar un segundo
-        self.move_forward(0)  # Mover 4 unidades hacia adelante
-        
+  // Giramos a la izquierda (activando solo el motor2) durante 2 segundos
+  digitalWrite(motor2APin, LOW);
+  digitalWrite(motor2BPin, HIGH);
+  delay(2000); // Giramos durante 2 segundos
 
-if _name_ == '_main_':
-    try:
-        move_turtle = MoveTurtle()
-        move_turtle.move_turtle()
-    except rospy.ROSInterruptException:
-        pass
+  // Detenemos el motor2
+  digitalWrite(motor2BPin, LOW);
+  delay(1000); // Esperamos un segundo
+}
